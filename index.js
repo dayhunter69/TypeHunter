61e112b58064dc1e06aaef48ae1a696f677b2867
anilkunwar <dayhunter69@gmail.com>
2024-03-25 01:48:54 +0545

    added the backspace feature to invalidate letters

--- index.js
+++ index.js
@@ -1,5 +1,5 @@
 const words =
-  "for if else while function return int float double char string class object array list dictionary import from def print input output true false null true false try except catch finally raise throw break continue new delete static final const let var async await promise resolve reject constructor extends implements interface public private protected static final abstract override virtual static_cast dynamic_cast typeof instanceof this super self module package require export global static inline constexpr typedef namespace using template friend struct union enum volatile register asm goto volatile".split(
+  "for if else while function return int float double char string class object array list dictionary import from def print input output true false null true false try except catch finally raise throw break continue new delete static final const let var async await promise resolve reject constructor extends implements interface public private protected static final abstract override virtual  typeof instanceof this super self module package require export global static inline constexpr typedef namespace using template friend struct union enum volatile register asm goto volatile".split(
     " "
   );
 
@@ -39,6 +39,8 @@ document.getElementById("game").addEventListener("keyup", (ev) => {
   const expected = currentLetter?.innerHTML || " ";
   const isLetter = key.length === 1 && key.match(/[a-z]/i);
   const isSpace = key === " ";
+  const isBackspace = key === "Backspace";
+  const isFirstLetter = currentLetter === currentWord.firstChild;
   console.log({ key, expected });
   if (isLetter) {
     if (currentLetter) {
@@ -48,6 +50,11 @@ document.getElementById("game").addEventListener("keyup", (ev) => {
       if (currentLetter.nextSibling) {
         addClass(currentLetter.nextSibling, "current");
       }
+    } else {
+      const incorrectLetter = document.createElement("span");
+      incorrectLetter.innerHTML = key;
+      incorrectLetter.className = "letter incorrect extra";
+      currentWord.appendChild(incorrectLetter);
     }
   }
   if (isSpace) {
@@ -64,6 +71,50 @@ document.getElementById("game").addEventListener("keyup", (ev) => {
     }
     addClass(currentWord.nextSibling.firstChild, "current");
   }
+
+  if (isBackspace) {
+    if (currentLetter && isFirstLetter) {
+      //make previous work current, last letter current
+      removeClass(currentWord, "current");
+      addClass(currentWord.previousSibling, "current");
+      removeClass(currentLetter, "current");
+      addClass(currentWord.previousSibling.lastChild, "current");
+      removeClass(currentWord.previousSibling.lastChild, "incorrect");
+      removeClass(currentWord.previousSibling.lastChild, "correct");
+    }
+    if (currentLetter && !isFirstLetter) {
+      removeClass(currentLetter, "current");
+      addClass(currentLetter.previousSibling, "current");
+      removeClass(currentLetter.previousSibling, "incorrect");
+      removeClass(currentLetter.previousSibling, "correct");
+    }
+    if (!currentLetter) {
+      addClass(currentWord.lastChild, "current");
+      removeClass(currentWord.lastChild, "correct");
+      removeClass(currentWord.lastChild, "incorrect");
+    }
+  }
+
+  //move curosr
+  const nextLetter = document.querySelector(".letter.current");
+  const nextWord = document.querySelector(".word.current");
+  const cursor = document.getElementById("cursor");
+  //refactoring the if else
+  cursor.style.top =
+    (nextLetter || nextWord).getBoundingClientRect().top +
+    (nextLetter ? "0" : 8) +
+    "px";
+  cursor.style.left =
+    (nextLetter || nextWord).getBoundingClientRect()[
+      nextLetter ? "left" : "right"
+    ] + "px";
+  // if (nextLetter) {
+  //   cursor.style.top = nextLetter.getBoundingClientRect().top + "px";
+  //   cursor.style.left = nextLetter.getBoundingClientRect().left + "px";
+  // } else {
+  //   cursor.style.top = nextWord.getBoundingClientRect().top + 8 + "px";
+  //   cursor.style.left = nextWord.getBoundingClientRect().right + "px";
+  // }
 });
 
 newGame();
